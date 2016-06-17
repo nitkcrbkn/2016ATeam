abb4c25c5d026245f6260c60f3f84f1ba398c1de
EVAOTA <nervevawaka@ezweb.ne.jp>
2016-06-17 18:36:48 +0900

    delete file

--- obj/xprintf.d
+++
@@ -1,4 +0,0 @@
-obj/xprintf.o: Drivers/middleLayers/message/Src/xprintf.c \
- /home/evaota/STM32F1_HAL/stm32f103_Test/Drivers/middleLayers/message/Inc/xprintf.h
-
-/home/evaota/STM32F1_HAL/stm32f103_Test/Drivers/middleLayers/message/Inc/xprintf.h:
