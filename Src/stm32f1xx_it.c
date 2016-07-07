6f3a9d996d7b4b468adfc6cb8f813b8460817129
Gombe <kenicecandy0141@outlook.com>
2016-07-07 16:54:38 +0900

    rename variable systemCounter

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -167,7 +167,7 @@ void SysTick_Handler(void)
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   HAL_SYSTICK_IRQHandler();
-  systemCounter++;
+  SY_systemCounter++;
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
