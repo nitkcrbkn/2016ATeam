f8c771cf240e1696264220f7191c6fecdb2037ca
Gombe <kenicecandy0141@outlook.com>
2016-07-10 12:19:52 +0900

    アプリケーションの例を検証用として追加

--- App/Inc/app.h
+++ App/Inc/app.h
@@ -1,6 +1,16 @@
 #ifndef __APP_H
 #define __APP_H
 
+#include "DD_RC.h"
+#include "DD_MD.h"
+
 int appTask(void);
+
+#define MECHA1_MD1 0
+#define MECHA1_MD2 1
+#define CENTRAL_THRESHOLD 3
+
+
+#define MD_GAIN ( DD_MD_MAX_DUTY / DD_RC_ANALOG_MAX )
 
 #endif
--- App/Src/app.c
+++ App/Src/app.c
@@ -1,7 +1,63 @@
+#include "DD_Gene.h"
+#include "DD_RC.h"
+#include "DD_RCDefinition.h"
+#include "SystemTaskManager.h"
 #include "app.h"
 #include <stdlib.h>
 
-int appTask(void){
+/*足回り*/
+static
+int suspentionSystem(void);
 
+/*メモ
+ *g_ab_h...ABのハンドラ
+ *g_md_h...MDのハンドラ
+ *
+ *g_rc_data...RCのデータ
+ */
+
+int appTask(void){
+  int ret;
+
+  ret = suspentionSystem();
+  if(ret){
+    return ret;
+  }
+  
   return EXIT_SUCCESS;
 }
+
+static
+int suspentionSystem(void){
+  const int num_of_motor = 2;
+  int rc_analogdata;
+  unsigned int idx;
+  int i;
+
+  /*for each motor*/
+  for(i=0;i<num_of_motor;i++){
+    /*diff*/
+    switch(i){
+    case 0: rc_analogdata = DD_RCGetRY(g_rc_data);idx = MECHA1_MD1;break;
+    case 1: rc_analogdata = DD_RCGetLY(g_rc_data);idx = MECHA1_MD2;break;
+    default:return EXIT_FAILURE;
+    }
+
+    /*is in centre?*/
+    if(abs(rc_analogdata)<CENTRAL_THRESHOLD){
+      g_md_h[idx].mode = D_MMOD_FREE;
+    }
+    else{
+      if(rc_analogdata > 0){
+	g_md_h[idx].mode = D_MMOD_FORWARD;
+      }
+      else{
+	g_md_h[idx].mode = D_MMOD_BACKWARD;
+      }
+      g_md_h[idx].duty = abs(rc_analogdata) * MD_GAIN;
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
+
