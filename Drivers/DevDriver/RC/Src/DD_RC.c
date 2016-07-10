3ba85575e3957fe4b8c2509be389d48d1056242d
Gombe <kenicecandy0141@outlook.com>
2016-07-10 10:49:02 +0900

    定義を移動

--- Drivers/DevDriver/RC/Src/DD_RC.c
+++ Drivers/DevDriver/RC/Src/DD_RC.c
@@ -1,4 +1,5 @@
 #include "DD_RC.h"
+#include "DD_RCDefinition.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include "message.h"
@@ -6,16 +7,23 @@
 #include "MW_USART.h"
 
 static volatile int is_not_avaiable = 8;
-static volatile uint8_t correct[8];
+static volatile uint8_t correct[RC_DATA_NUM];
 
-int DD_RCInit(uint8_t rc_data[8],uint32_t timeout){
-  uint32_t remain;
-  MW_USARTInit(USART3ID);
+int DD_RCInit(uint8_t rc_data[RC_DATA_NUM],uint32_t timeout){
+  uint32_t remaining;
+  int ret;
 
-  remain = SY_systemCounter+timeout;
+  MW_USARTSetBaudRate(USART3ID,_RC_BAUD);
+  ret = MW_USARTInit(USART3ID);
+  if(ret){
+    return ret;
+  }
+  
+  remaining = g_SY_system_counter+timeout;
 
-  MW_USART3ReceiveMult(8,rc_data);
-  while(remain>SY_systemCounter&&is_not_avaiable);
+  MW_USART3ReceiveMult(RC_DATA_NUM,rc_data);
+  while(remaining>g_SY_system_counter&&is_not_avaiable);
+
   if(is_not_avaiable){
     return EXIT_FAILURE;
   }
@@ -23,11 +31,11 @@ int DD_RCInit(uint8_t rc_data[8],uint32_t timeout){
   return EXIT_SUCCESS;
 }
 
-void DD_RCTask(const uint8_t rc_data[8],uint8_t out_data[8]){
+void DD_RCTask(const uint8_t rc_data[RC_DATA_NUM],uint8_t out_data[RC_DATA_NUM]){
   int i;
   if(is_not_avaiable){
     is_not_avaiable--;
-    for(i=0;i<8;i++){
+    for(i=0;i<RC_DATA_NUM;i++){
       correct[rc_data[i]>>5]+=rc_data[i]&(0xFF>>3);
     }
     if(!is_not_avaiable){
@@ -38,30 +46,30 @@ void DD_RCTask(const uint8_t rc_data[8],uint8_t out_data[8]){
     }
   }
   else{
-    for(i=0;i<8;i++){
+    for(i=0;i<RC_DATA_NUM;i++){
       out_data[rc_data[i]>>5]=rc_data[i]&(0xFF>>3);
     }
   }
   MW_USART3ReceiveMult(8,rc_data);
 }
 
-int DD_RCGetLX(uint8_t data[8]){
+int DD_RCGetLX(uint8_t data[RC_DATA_NUM]){
   return data[__RC_LX]-correct[__RC_LX];
 }
 
-int DD_RCGetLY(uint8_t data[8]){
+int DD_RCGetLY(uint8_t data[RC_DATA_NUM]){
   return data[__RC_LY]-correct[__RC_LY];
 }
 
-int DD_RCGetRX(uint8_t data[8]){
+int DD_RCGetRX(uint8_t data[RC_DATA_NUM]){
   return data[__RC_RX]-correct[__RC_RX];
 }
 
-int DD_RCGetRY(uint8_t data[8]){
+int DD_RCGetRY(uint8_t data[RC_DATA_NUM]){
   return data[__RC_RY]-correct[__RC_RY];
 }
 
-int DD_RCPrint(uint8_t data[8]){
+int DD_RCPrint(uint8_t data[RC_DATA_NUM]){
   if(is_not_avaiable)return EXIT_FAILURE;
   MW_printf("RC btn:[");
   if(__RC_ISPRESSED_UP(data)){
