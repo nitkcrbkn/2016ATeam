53b52d33789e84ec280b9b54cc3b3a5d137efcab
KenOishi <kenicecandy0141@yahoo.co.jp>
2016-07-11 19:04:14 +0900

    レンジを設定、プロトタイプの修正

--- Drivers/DevDriver/RC/Inc/DD_RC.h
+++ Drivers/DevDriver/RC/Inc/DD_RC.h
@@ -5,7 +5,7 @@
 #define RC_DATA_NUM 8
 
 int DD_RCInit(uint8_t setdata[RC_DATA_NUM],uint32_t timeout);
-void DD_RCTask(const uint8_t rcv_data[RC_DATA_NUM],uint8_t setdata[RC_DATA_NUM]);
+void DD_RCTask(uint8_t rcv_data[RC_DATA_NUM],uint8_t setdata[RC_DATA_NUM]);
 int DD_RCPrint(volatile uint8_t data[RC_DATA_NUM]);
 
 /*アナログデータ取得*/
--- Drivers/DevDriver/RC/Src/DD_RC.c
+++ Drivers/DevDriver/RC/Src/DD_RC.c
@@ -6,8 +6,10 @@
 #include "SystemTaskManager.h"
 #include "MW_USART.h"
 
-static volatile int is_not_avaiable = 8;
+static volatile int is_not_avaiable = 8+1;
 static volatile uint8_t correct[RC_DATA_NUM];
+
+#define _RELANGE(x) (x<15?(x>-15?x:-15):15)
 
 int DD_RCInit(uint8_t rc_data[RC_DATA_NUM], uint32_t timeout){
   uint32_t remaining;
@@ -32,17 +34,18 @@ int DD_RCInit(uint8_t rc_data[RC_DATA_NUM], uint32_t timeout){
   return EXIT_SUCCESS;
 }
 
-void DD_RCTask(const uint8_t rc_data[RC_DATA_NUM], uint8_t out_data[RC_DATA_NUM]){
+void DD_RCTask(uint8_t rc_data[RC_DATA_NUM], uint8_t out_data[RC_DATA_NUM]){
   int i;
-  if( is_not_avaiable ){
+  
+  if( is_not_avaiable > 0){
     is_not_avaiable--;
     for( i = 0; i < RC_DATA_NUM; i++ ){
       correct[rc_data[i] >> 5] += rc_data[i] & ( 0xFF >> 3 );
     }
     if( !is_not_avaiable ){
-      for( i = 0; i < 4; i++ ){
+      for( i = 0; i < RC_DATA_NUM; i++ ){
         /*calc average*/
-        correct[i + 4] <<= 3;
+        correct[i] >>= 3;
       }
     }
   }else  {
@@ -54,19 +57,19 @@ void DD_RCTask(const uint8_t rc_data[RC_DATA_NUM], uint8_t out_data[RC_DATA_NUM]
 }
 
 int DD_RCGetLX(volatile uint8_t data[RC_DATA_NUM]){
-  return data[__RC_LX] - correct[__RC_LX];
+  return _RELANGE((int)data[__RC_LX] - (int)correct[__RC_LX]);
 }
 
 int DD_RCGetLY(volatile uint8_t data[RC_DATA_NUM]){
-  return data[__RC_LY] - correct[__RC_LY];
+  return _RELANGE((int)data[__RC_LY] - (int)correct[__RC_LY]);
 }
 
 int DD_RCGetRX(volatile uint8_t data[RC_DATA_NUM]){
-  return data[__RC_RX] - correct[__RC_RX];
+  return _RELANGE((int)data[__RC_RX] - (int)correct[__RC_RX]);
 }
 
 int DD_RCGetRY(volatile uint8_t data[RC_DATA_NUM]){
-  return data[__RC_RY] - correct[__RC_RY];
+  return _RELANGE((int)data[__RC_RY] - (int)correct[__RC_RY]);
 }
 
 int DD_RCPrint(volatile uint8_t data[RC_DATA_NUM]){
@@ -123,4 +126,3 @@ int DD_RCPrint(volatile uint8_t data[RC_DATA_NUM]){
 
   return EXIT_SUCCESS;
 } /* DD_RCPrint */
-
