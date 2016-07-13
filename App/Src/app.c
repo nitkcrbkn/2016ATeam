b8573f9e5ead14e6411b4fa2e160bdc3e4707226
KenOishi <kenicecandy0141@yahoo.co.jp>
2016-07-13 17:52:54 +0900

    デバイス定義を移動

--- App/Src/app.c
+++ App/Src/app.c
@@ -5,29 +5,6 @@
 #include <stdlib.h>
 #include "message.h"
 
-/*Address Definition*/
-#if DD_NUM_OF_MD
-/*MD Definition*/
-DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
-  { 0x10, /* address */
-    0, /* default duty */
-    D_MMOD_FREE, /* mode */
-  },
-  { 0x10, /* address */
-    0, /* default duty */
-    D_MMOD_FREE, /* mode */
-  },
-};
-#endif
-#if DD_NUM_OF_AB
-/*AB Definition*/
-DD_ABHand_t g_ab_h[DD_NUM_OF_AB] = {
-  { 0x11, /* address */
-    0x00, /* data */
-  },
-};
-#endif
-
 /*suspentionSystem*/
 static
 int suspentionSystem(void);
@@ -123,5 +100,3 @@ int suspentionSystem(void){
   }
   return EXIT_SUCCESS;
 }
-
-
---
+++ App/Src/deviceDefinition.c
@@ -0,0 +1,26 @@
+#include "DD_Gene.h"
+#include "app.h"
+#include "SystemTaskManager.h"
+
+/*Address Definition*/
+#if DD_NUM_OF_MD
+/*MD Definition*/
+DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
+  { 0x10, /* address */
+    0, /* default duty */
+    D_MMOD_FREE, /* mode */
+  },
+  { 0x10, /* address */
+    0, /* default duty */
+    D_MMOD_FREE, /* mode */
+  },
+};
+#endif
+#if DD_NUM_OF_AB
+/*AB Definition*/
+DD_ABHand_t g_ab_h[DD_NUM_OF_AB] = {
+  { 0x11, /* address */
+    0x00, /* data */
+  },
+};
+#endif
