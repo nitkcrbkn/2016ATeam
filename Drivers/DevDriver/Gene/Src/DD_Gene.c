6f8c659ef65ea53e12d36cc546bf59b9ef584e65
Gombe <kenicecandy0141@outlook.com>
2016-07-16 12:39:25 +0900

    I2Cの初期化の追加とプロトタイプの変更

--- Drivers/DevDriver/Gene/Src/DD_Gene.c
+++ Drivers/DevDriver/Gene/Src/DD_Gene.c
@@ -60,6 +60,15 @@ void DD_print(void){
 #endif
 }
 
-void DD_Initialize(void){
+int DD_initialize(void){
   message("msg","device initialize\n");
+  
+  /* Initialize all configured peripherals */
+  MW_SetI2CClockSpeed(I2C1ID, _I2C_SPEED_BPS);
+  ret = MW_I2CInit(I2C1ID);
+  if( ret ){
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
