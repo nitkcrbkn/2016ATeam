adaf341990dff2937cc69c9ca7e7c239853cd873
semi11 <stand.say.me@gmai.com>
2016-07-16 10:26:31 +0900

    MDのアドレスを振り直した

--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -10,7 +10,7 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
     0, /* default duty */
     D_MMOD_FREE, /* mode */
   },
-  { 0x10, /* address */
+  { 0x11, /* address */
     0, /* default duty */
     D_MMOD_FREE, /* mode */
   },
