e681f74212fb6c1c29847ae598b98b628edb2e5b
semi11 <stand.say.me@gmai.com>
2016-07-19 16:20:04 +0900

    台形制御を実装

--- App/Src/app.c
+++ App/Src/app.c
@@ -42,8 +42,8 @@ int appTask(void){
 static
 int suspensionSystem(void){
   const int num_of_motor = 2;/*モータの個数*/
-  const int rising_val = 20;/* 立ち上がり値 */
-  const int falling_val = 20;/* 立ち下がり値 */
+  const int rising_val = 200;/* 立ち上がり値 */
+  const int falling_val = 200;/* 立ち下がり値 */
   int rc_analogdata;/*アナログデータ*/
   int ctrl_val;/* 制御値 */
   unsigned int idx;/*インデックス*/
@@ -73,37 +73,49 @@ int suspensionSystem(void){
     default:return EXIT_FAILURE;
     }
 
-    /*これは中央か?±3程度余裕を持つ必要がある。*/
+   /*これは中央か?±3程度余裕を持つ必要がある。*/
     if(abs(rc_analogdata)<CENTRAL_THRESHOLD){
-      // g_md_h[idx].mode = D_MMOD_FREE;
       target_duty=0;
     }else{
-      target_duty=abs(rc_analogdata) * md_gain;
+      target_duty=rc_analogdata * md_gain;
     }
-    
-    if(rc_analogdata > 0){
-      /*前後の向き判定*/
-      g_md_h[idx].mode = D_MMOD_FORWARD;
-    }
-    else{
-      g_md_h[idx].mode = D_MMOD_BACKWARD;
-    }
-
-    /*絶対値を取りDutyに格納*/
     prev_duty=g_md_h[idx].duty;
 
-    if(prev_duty < target_duty){
-      ctrl_val=prev_duty+min2(rising_val,target_duty-prev_duty);
-    }else if(prev_duty > target_duty){
-      ctrl_val=prev_duty-min2(falling_val,prev_duty-target_duty);
+    /* 台形制御 */
+    switch(g_md_h[idx].mode){
+    case D_MMOD_FREE:
+    case D_MMOD_BRAKE:
+    case D_MMOD_FORWARD:
+      if(prev_duty < target_duty){ 
+    	ctrl_val=prev_duty+min2(rising_val,target_duty-prev_duty);
+      }else if(prev_duty > target_duty){
+    	ctrl_val=prev_duty-min2(falling_val,prev_duty-target_duty);
+      }else ctrl_val=target_duty;
+      break;
+    case D_MMOD_BACKWARD:
+      prev_duty*=-1;
+      if(prev_duty > target_duty){ 
+    	ctrl_val=prev_duty-min2(rising_val,prev_duty-target_duty);
+      }else if(prev_duty < target_duty){
+    	ctrl_val=prev_duty+min2(falling_val,target_duty-prev_duty);
+      }else ctrl_val=target_duty;
+      break;
+    default:return EXIT_FAILURE;
+    }
+    
+    /*前後の向き判定*/
+    if(ctrl_val > 0){
+      g_md_h[idx].mode = D_MMOD_FORWARD;
+    }else if(ctrl_val < 0){
+      g_md_h[idx].mode = D_MMOD_BACKWARD;
     }else{
-      ctrl_val=target_duty;
+      g_md_h[idx].mode = D_MMOD_BRAKE;
     }
 
-    g_md_h[idx].duty = ctrl_val;
-   
+    g_md_h[idx].duty = abs(ctrl_val);
+    
   }
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
