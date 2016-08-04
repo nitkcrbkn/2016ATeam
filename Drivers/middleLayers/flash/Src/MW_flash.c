557359ea489dd20d10480a8dc3c25e885ec9219d
elect-gombe <kenicecandy0141@outlook.com>
2016-08-04 18:57:39 +0900

    定数のミス、照合次の数字のミスの修正

--- Drivers/middleLayers/flash/Src/MW_flash.c
+++ Drivers/middleLayers/flash/Src/MW_flash.c
@@ -11,13 +11,13 @@ flashError_t MW_flashUnlock(void);
 static
 flashError_t MW_flashLock(void);
 static
-flashError_t MW_flashElase(/*!erase here!*/const void *flash_add,size_t size);
+flashError_t MW_flashElase(/*!erase here!*/ const void *flash_add, size_t size);
 
 static
 FLASH_EraseInitTypeDef elase_hand;
 
 static
-flashError_t MW_flashElase(/*!erase here!*/const void *flash_add,size_t size){
+flashError_t MW_flashElase(/*!erase here!*/ const void *flash_add, size_t size){
   /*消去から。一ページ削除。周辺にコードを置かないようにリンカスクリプトを変更すること。*/
   {
     uint32_t page_err;
@@ -25,16 +25,17 @@ flashError_t MW_flashElase(/*!erase here!*/const void *flash_add,size_t size){
     elase_hand.TypeErase = FLASH_TYPEERASE_PAGES;
     elase_hand.NbPages = 1;
     elase_hand.PageAddress = (uint32_t)flash_add;
-    if(HAL_FLASHEx_Erase(&elase_hand, &page_err)!=HAL_ERROR){
+    if( HAL_FLASHEx_Erase(&elase_hand, &page_err) != HAL_OK ){
       return MW_FLASH_ERASE_FAILURE;
     }
   }
 
   {
     uint32_t i;
-    for(i=0;i<size;i++){
-      if(((uint8_t*)flash_add)[i]!=0)
-	return MW_FLASH_ERASE_VERIFY_FAILURE;
+    for( i = 0; i < size / 2; i++ ){
+      if(((uint16_t*)flash_add )[i] != 0xFFFF ){
+        return MW_FLASH_ERASE_VERIFY_FAILURE;
+      }
     }
   }
   return MW_FLASH_OK;
@@ -42,41 +43,50 @@ flashError_t MW_flashElase(/*!erase here!*/const void *flash_add,size_t size){
 
 static
 flashError_t MW_flashUnlock(void){
-  if(HAL_FLASH_Unlock()!=HAL_OK)return MW_FLASH_UNLOCK_FAILURE;
+  if( HAL_FLASH_Unlock() != HAL_OK ){
+    return MW_FLASH_UNLOCK_FAILURE;
+  }
   return MW_FLASH_OK;
 }
 
 static
 flashError_t MW_flashLock(void){
-  if(HAL_FLASH_Lock()!=HAL_OK)return MW_FLASH_LOCK_FAILURE;
+  if( HAL_FLASH_Lock() != HAL_OK ){
+    return MW_FLASH_LOCK_FAILURE;
+  }
   return MW_FLASH_OK;
 }
 
-flashError_t MW_flashWrite(const void *ptr,/*!write here!*/const void *flash_add,size_t size){
+flashError_t MW_flashWrite(const void *ptr, /*!write here!*/ const void *flash_add, size_t size){
   uint32_t address;
   flashError_t err;
 
   err = MW_flashUnlock();
-  if(err != MW_FLASH_OK)return err;
-  
-  /*消去から。一ページ削除。周辺にコードを置かないようにリンカスクリプトを変更すること。*/
-  err = MW_flashElase(flash_add, size);
-  if(err != MW_FLASH_OK){
+  if( err != MW_FLASH_OK ){
     return err;
   }
-  
+
+  /*消去から。一ページ削除。周辺にコードを置かないようにリンカスクリプトを変更すること。*/
+  err = MW_flashElase(flash_add, size);
+  if( err != MW_FLASH_OK ){
+    return err;
+  }
+
   address = (uint32_t)flash_add;
-  for(;address + 1 < (uint32_t)flash_add + size;address+=2){
-    if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,address,*(const uint16_t*)ptr)!=HAL_OK){
+  for(; address + 1 < (uint32_t)flash_add + size; address += 2 ){
+    if( HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, address, *(const uint16_t*)ptr) != HAL_OK ){
       return MW_FLASH_WRITE_FAILURE;
     }
-    if(*(const uint16_t*)ptr++!=*(uint16_t*)address){
+    if( *(const uint16_t*)ptr++ != *(uint16_t*)address ){
       return MW_FLASH_WRITE_VERIFY_FAILURE;
     }
-  }  
+  }
 
   err = MW_flashLock();
-  if(err != MW_FLASH_OK)return err;
-  
+  if( err != MW_FLASH_OK ){
+    return err;
+  }
+
   return EXIT_SUCCESS;
-}
+} /* MW_flashWrite */
+
