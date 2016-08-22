128218ac5f488d9a861bc283d5adaf1d693274fe
elect-gombe <kenicecandy0141@yahoo.co.jp>
2016-08-22 12:47:07 +0900

    不要なテストコードを削除
    サンプルを別途作成しました。81654ebc30d1905ca42db7682963d9eb13d0accb

--- App/Src/app.c
+++ App/Src/app.c
@@ -21,43 +21,8 @@ int ABSystem(void);
  *g_rc_data...RCのデータ
  */
 
-#define WRITE_ADDR (const void*)(0x8000000+0x400*(128-1))/*128[KiB]*/
-flashError_t checkFlashWrite(void){
-  const char data[]="HelloWorld!!TestDatas!!!\n"
-    "however you like this microcomputer, you don`t be kind to this computer.";
-  return MW_flashWrite(data,WRITE_ADDR,sizeof(data));
-}
-
 int appInit(void){
   message("msg","hell");
-
-  /* switch(checkFlashWrite()){ */
-  /* case MW_FLASH_OK: */
-  /*   message("msg","FLASH WRITE TEST SUCCESS\n%s",(const char*)WRITE_ADDR); */
-  /*   break; */
-  /* case MW_FLASH_LOCK_FAILURE: */
-  /*   message("err","FLASH WRITE TEST LOCK FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_UNLOCK_FAILURE: */
-  /*   message("err","FLASH WRITE TEST UNLOCK FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_ERASE_VERIFY_FAILURE: */
-  /*   message("err","FLASH ERASE VERIFY FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_ERASE_FAILURE: */
-  /*   message("err","FLASH ERASE FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_WRITE_VERIFY_FAILURE: */
-  /*   message("err","FLASH WRITE TEST VERIFY FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_WRITE_FAILURE: */
-  /*   message("err","FLASH WRITE TEST FAILURE\n"); */
-  /*   break;         */
-  /* default: */
-  /*   message("err","FLASH WRITE TEST UNKNOWN FAILURE\n"); */
-  /*   break; */
-  /* } */
-  /* flush(); */
 
   ad_init();
 
