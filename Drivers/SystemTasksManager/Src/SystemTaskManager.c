4ba1adb0b5b99285cfa54256ffc5f8ea5722245e
EVAOTA <nervevawaka@ezweb.ne.jp>
2016-08-23 11:19:50 +0900

    RCチェックの無効化

--- Drivers/SystemTasksManager/Src/SystemTaskManager.c
+++ Drivers/SystemTasksManager/Src/SystemTaskManager.c
@@ -146,13 +146,13 @@ int SY_init(void){
 
   appInit();
 
-  message("msg", "wait for RC connection...");
+  /*  message("msg", "wait for RC connection...");
   if( DD_RCInit((uint8_t*)g_rc_data, 10000) ){
     message("err", "RC initialize faild!\n");
     return EXIT_FAILURE;
   }
   message("msg", "RC connected sucess");
-
+  */
   appInit();
 
   return EXIT_SUCCESS;
