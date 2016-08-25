f89bd813e1e2a87a642ba7f76c219070fbf53302
semi11 <stand.say.me@gmai.com>
2016-08-25 11:17:50 +0900

    コードを整形

--- App/Src/app.c
+++ App/Src/app.c
@@ -24,11 +24,10 @@ int ABSystem(void);
  * *g_ab_h...ABのハンドラ
  * *g_md_h...MDのハンドラ
  *
- **g_rc_data...RCのデータ
+ *  g_rc_data...RCのデータ
  */
 
 int appInit(void){
-
   ad_init();
 
   /*GPIO の設定などでMW,GPIOではHALを叩く*/
@@ -39,13 +38,14 @@ int appInit(void){
 int appTask(void){
   int ret = 0;
 
-  if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_R2(g_rc_data)&&
-     __RC_ISPRESSED_L1(g_rc_data)&&__RC_ISPRESSED_L2(g_rc_data)){
-    while(__RC_ISPRESSED_R1(g_rc_data)||__RC_ISPRESSED_R2(g_rc_data)||
-	  __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
+  if( __RC_ISPRESSED_R1(g_rc_data) && __RC_ISPRESSED_R2(g_rc_data) &&
+      __RC_ISPRESSED_L1(g_rc_data) && __RC_ISPRESSED_L2(g_rc_data)){
+    while( __RC_ISPRESSED_R1(g_rc_data) || __RC_ISPRESSED_R2(g_rc_data) ||
+           __RC_ISPRESSED_L1(g_rc_data) || __RC_ISPRESSED_L2(g_rc_data)){
+    }
     ad_main();
   }
-  
+
   /*それぞれの機構ごとに処理をする*/
   /*途中必ず定数回で終了すること。*/
   ret = suspensionSystem();
@@ -69,19 +69,19 @@ int appTask(void){
 /*プライベート 足回りシステム*/
 static
 int suspensionSystem(void){
-  const int num_of_motor = 3; /*モータの個数*/
-  const int rising_val = 200; /* 立ち上がり値 */
-  const int falling_val = 200; /* 立ち下がり値 */
-  int rc_analogdata; /*アナログデータ*/
-  int is_reverse; /* 反転するか */ 
-  unsigned int idx; /*インデックス*/
-  unsigned int md_gain; /*アナログデータの補正値 */
-  int ctl_motor_kind; /* 現在制御しているモータ */
-  int target_val; /* 目標値 */
+  const int num_of_motor = 3;       /*モータの個数*/
+  const int rising_val = 200;       /* 立ち上がり値 */
+  const int falling_val = 200;       /* 立ち下がり値 */
+  int rc_analogdata;       /*アナログデータ*/
+  int is_reverse;       /* 反転するか */
+  unsigned int idx;       /*インデックス*/
+  unsigned int md_gain;       /*アナログデータの補正値 */
+  int ctl_motor_kind;       /* 現在制御しているモータ */
+  int target_val;       /* 目標値 */
 
   /*for each motor*/
   for( ctl_motor_kind = ROB1_DRIL; ctl_motor_kind < num_of_motor; ctl_motor_kind++ ){
-    is_reverse=0;  
+    is_reverse = 0;
 
     /*それぞれの差分*/
     switch( ctl_motor_kind ){
@@ -90,7 +90,7 @@ int suspensionSystem(void){
       md_gain = MD_GAIN_DRIL;
       /* 前後の向きを反転 */
 #if _IS_REVERSE_DRIL
-      is_reverse=1;
+      is_reverse = 1;
 #endif
       idx = ROB1_DRIL;
       break;
@@ -99,7 +99,7 @@ int suspensionSystem(void){
       md_gain = MD_GAIN_DRIR;
       /* 前後の向きを反転 */
 #if _IS_REVERSE_DRIR
-      is_reverse=1;
+      is_reverse = 1;
 #endif
       idx = ROB1_DRIR;
       break;
@@ -108,13 +108,13 @@ int suspensionSystem(void){
       md_gain = MD_GAIN_DRIB;
       /* 前後の向きを反転 */
 #if _IS_REVERSE_DRIB
-      is_reverse=1;
+      is_reverse = 1;
 #endif
       idx = ROB1_DRIB;
       break;
     default: return EXIT_FAILURE;
     }
-    
+
     /* 目標値計算 */
     /*これは中央か?±3程度余裕を持つ必要がある。*/
     if( abs(rc_analogdata) < CENTRAL_THRESHOLD ){
@@ -124,26 +124,27 @@ int suspensionSystem(void){
     }
 
     /* モータの回転を反転 */
-    if(is_reverse) target_val = -target_val;
+    if( is_reverse ){
+      target_val = -target_val;
+    }
 
-    control_trapezoid(rising_val ,falling_val ,&g_md_h[idx] ,target_val);
-
+    control_trapezoid(rising_val, falling_val, &g_md_h[idx], target_val);
   }
   return EXIT_SUCCESS;
 } /* suspensionSystem */
 
 int pushSystem(void){
-  if(__RC_ISPRESSED_UP(g_rc_data)){
+  if( __RC_ISPRESSED_UP(g_rc_data)){
 #if _REVERSE_PSH
     g_md_h[ROB1_PSH].mode = D_MMOD_FORWARD;
-#else 
+#else
     g_md_h[ROB1_PSH].mode = D_MMOD_BACKWARD;
 #endif
     g_md_h[ROB1_PSH].duty = MD_MAX_DUTY_PSH;
-  }else if(__RC_ISPRESSED_DOWN(g_rc_data)){
+  }else if( __RC_ISPRESSED_DOWN(g_rc_data)){
 #if _REVERSE_PSH
     g_md_h[ROB1_PSH].mode = D_MMOD_BACKWARD;
-#else 
+#else
     g_md_h[ROB1_PSH].mode = D_MMOD_FORWARD;
 #endif
     g_md_h[ROB1_PSH].duty = MD_MAX_DUTY_PSH;
@@ -156,30 +157,33 @@ int pushSystem(void){
 }
 
 int ABSystem(void){
-  
   static int has_pressed_R1;
   static int has_pressed_L1;
-  
-  if(__RC_ISPRESSED_R1(g_rc_data)){
-    if(!has_pressed_R1){//R1が押され続けている間は処理を行わない
+
+  if( __RC_ISPRESSED_R1(g_rc_data)){
+    if( !has_pressed_R1 ){            /* R1が押され続けている間は処理を行わない */
       has_pressed_R1 = 1;
-      if((g_ab_h[AB].dat & (LIFTL|LIFTR)) != (LIFTL|LIFTR)){//R1がすでに押されているか
-	g_ab_h[AB].dat |= (LIFTL|LIFTR);
+      if(( g_ab_h[AB].dat & ( LIFTL | LIFTR )) != ( LIFTL | LIFTR )){           /*
+                                                                                 *R1がすでに押されているか
+                                                                                 **/
+        g_ab_h[AB].dat |= ( LIFTL | LIFTR );
       }else{
-	g_ab_h[AB].dat &= ~(LIFTL|LIFTR);
+        g_ab_h[AB].dat &= ~( LIFTL | LIFTR );
       }
     }
   }else{
     has_pressed_R1 = 0;
   }
-  
-  if(__RC_ISPRESSED_L1(g_rc_data)){
-    if(!has_pressed_L1){//L1が押され続けている間は処理を行わない
+
+  if( __RC_ISPRESSED_L1(g_rc_data)){
+    if( !has_pressed_L1 ){            /* L1が押され続けている間は処理を行わない */
       has_pressed_L1 = 1;
-      if((g_ab_h[AB].dat & (PNCHL|PNCHR)) != (PNCHL|PNCHR)){//L1がすでに押されているか
-	g_ab_h[AB].dat |= (PNCHL|PNCHR);
+      if(( g_ab_h[AB].dat & ( PNCHL | PNCHR )) != ( PNCHL | PNCHR )){           /*
+                                                                                 *L1がすでに押されているか
+                                                                                 **/
+        g_ab_h[AB].dat |= ( PNCHL | PNCHR );
       }else{
-	g_ab_h[AB].dat &= ~(PNCHL|PNCHR);
+        g_ab_h[AB].dat &= ~( PNCHL | PNCHR );
       }
     }
   }else{
@@ -187,22 +191,5 @@ int ABSystem(void){
   }
 
   return EXIT_SUCCESS;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} /* ABSystem */
 
