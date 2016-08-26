53f7876db727b94f79bf61b699c84a7876ffa4d3
EVAOTA <nervevawaka@ezweb.ne.jp>
2016-08-26 10:39:15 +0900

    コード整形

--- Drivers/middleLayers/TIM/Inc/MW_TIM.h
+++ Drivers/middleLayers/TIM/Inc/MW_TIM.h
@@ -11,33 +11,32 @@
 #include "stm32f1xx_hal.h"
 #include <stdlib.h>
 
-typedef enum
-{
-	TIM1ID,
-	TIM2ID
+typedef enum {
+  TIM1ID,
+  TIM2ID
 }timid_t;
 
-#define IS_TIM_ID(ID) (((ID) == TIM1ID) || \
-		       ((ID) == TIM2ID))
+#define IS_TIM_ID(ID) ((( ID ) == TIM1ID ) || \
+                       (( ID ) == TIM2ID ))
 
-#define ENABLETIM1HANDLE(){\
-HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 1);\
-HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);\
-  }
-#define ENABLETIM2HANDLE(){\
-HAL_NVIC_SetPriority(TIM2_IRQn, 0, 1);\
-HAL_NVIC_EnableIRQ(TIM2_IRQn);\
-  }
+#define ENABLETIM1HANDLE(){ \
+    HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 1); \
+    HAL_NVIC_EnableIRQ(TIM1_UP_IRQn); \
+}
+#define ENABLETIM2HANDLE(){ \
+    HAL_NVIC_SetPriority(TIM2_IRQn, 0, 1); \
+    HAL_NVIC_EnableIRQ(TIM2_IRQn); \
+}
 
 int MW_TIMInit(timid_t id);
 
-void MW_SetTIMPrescaler(timid_t id,uint32_t Prescaler);
-void MW_SetTIMCounterMode(timid_t id,uint32_t CounterMode);
-void MW_SetTIMPeriod(timid_t id,uint32_t Period);
-void MW_SetTIMClockDivision(timid_t id,uint32_t ClockDivision);
-void MW_SetTIMClockSource(timid_t id,uint32_t ClockSource);
-void MW_SetTIMMasterOutputTrigger(timid_t id,uint32_t MasterOutputTrigger);
-void MW_SetTIMMasterSlaveMode(timid_t id,uint32_t MasterSlaveMode);
+void MW_SetTIMPrescaler(timid_t id, uint32_t Prescaler);
+void MW_SetTIMCounterMode(timid_t id, uint32_t CounterMode);
+void MW_SetTIMPeriod(timid_t id, uint32_t Period);
+void MW_SetTIMClockDivision(timid_t id, uint32_t ClockDivision);
+void MW_SetTIMClockSource(timid_t id, uint32_t ClockSource);
+void MW_SetTIMMasterOutputTrigger(timid_t id, uint32_t MasterOutputTrigger);
+void MW_SetTIMMasterSlaveMode(timid_t id, uint32_t MasterSlaveMode);
 
 void MW_TIMStartIT(timid_t id);
 void MW_TIMStopIT(timid_t id);
